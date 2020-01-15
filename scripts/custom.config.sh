450e2a801a4cfd3e9f1d654133931155a7dc8edd
bclayton-usgs <bclayton@usgs.gov>
2020-01-15 13:17:23 -0700

    add all models

--- scripts/custom.config.sh
+++ scripts/custom.config.sh
@@ -1,3 +1,8 @@
 export SERVICE_MAP=(
-  "/nshmp-haz":"nshmp-haz-cous-2018"
+  "/nshmp/hi-2020":"nshmp-haz-hi-2020"
+  "/nshmp/cous-2018":"nshmp-haz-cous-2018"
+  "/nshmp/cous-2014":"nshmp-haz-cous-2014"
+  "/nshmp/cous-2014b":"nshmp-haz-cous-2014b"
+  "/nshmp/cous-2008":"nshmp-haz-cous-2008"
+  "/nshmp/ak-2007":"nshmp-haz-ak-2007"
 );
