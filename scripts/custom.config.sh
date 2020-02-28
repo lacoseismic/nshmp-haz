718d3dc8e83d4faa8a90348b0a591a18c38a4f46
bclayton-usgs <bclayton@usgs.gov>
2020-02-28 16:25:24 -0700

    change deployment

--- scripts/custom.config.sh
+++ scripts/custom.config.sh
@@ -1,8 +1,8 @@
 export SERVICE_MAP=(
-  "/nshmp/hawaii-2020":"nshmp-haz-hi-2020"
   "/nshmp/conus-2018":"nshmp-haz-conus-2018"
-  "/nshmp/conus-2014":"nshmp-haz-conus-2014"
-  "/nshmp/conus-2014b":"nshmp-haz-conus-2014b"
-  "/nshmp/conus-2008":"nshmp-haz-conus-2008"
-  "/nshmp/alaska-2007":"nshmp-haz-ak-2007"
+  # "/nshmp/conus-2014":"nshmp-haz-conus-2014"
+  # "/nshmp/conus-2014b":"nshmp-haz-conus-2014b"
+  # "/nshmp/conus-2008":"nshmp-haz-conus-2008"
+  # "/nshmp/hawaii-2020":"nshmp-haz-hi-2020"
+  # "/nshmp/alaska-2007":"nshmp-haz-ak-2007"
 );
