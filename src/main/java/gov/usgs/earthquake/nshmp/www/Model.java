fbac5af045895e96d477049e29c395f2b469680f
bclayton-usgs <bclayton@usgs.gov>
2020-03-19 13:00:35 -0600

    Update to add CONUS

--- src/main/java/gov/usgs/earthquake/nshmp/www/Model.java
+++ src/main/java/gov/usgs/earthquake/nshmp/www/Model.java
@@ -1,104 +1,24 @@
 package gov.usgs.earthquake.nshmp.www;
 
-import static gov.usgs.earthquake.nshmp.calc.Vs30.VS_1150;
-import static gov.usgs.earthquake.nshmp.calc.Vs30.VS_180;
-import static gov.usgs.earthquake.nshmp.calc.Vs30.VS_2000;
-import static gov.usgs.earthquake.nshmp.calc.Vs30.VS_259;
-import static gov.usgs.earthquake.nshmp.calc.Vs30.VS_360;
-import static gov.usgs.earthquake.nshmp.calc.Vs30.VS_537;
-import static gov.usgs.earthquake.nshmp.calc.Vs30.VS_760;
-import static gov.usgs.earthquake.nshmp.gmm.Imt.PGA;
-import static gov.usgs.earthquake.nshmp.gmm.Imt.SA0P1;
-import static gov.usgs.earthquake.nshmp.gmm.Imt.SA0P2;
-import static gov.usgs.earthquake.nshmp.gmm.Imt.SA0P3;
-import static gov.usgs.earthquake.nshmp.gmm.Imt.SA0P5;
-import static gov.usgs.earthquake.nshmp.gmm.Imt.SA0P75;
-import static gov.usgs.earthquake.nshmp.gmm.Imt.SA1P0;
-import static gov.usgs.earthquake.nshmp.gmm.Imt.SA2P0;
-import static gov.usgs.earthquake.nshmp.gmm.Imt.SA3P0;
-import static gov.usgs.earthquake.nshmp.gmm.Imt.SA4P0;
-import static gov.usgs.earthquake.nshmp.gmm.Imt.SA5P0;
-import static gov.usgs.earthquake.nshmp.www.meta.Region.AK;
-import static gov.usgs.earthquake.nshmp.www.meta.Region.CEUS;
-import static gov.usgs.earthquake.nshmp.www.meta.Region.HI;
-import static gov.usgs.earthquake.nshmp.www.meta.Region.WUS;
-
-import java.nio.file.Paths;
-import java.util.EnumSet;
 import java.util.Set;
-
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.Sets;
-
-import gov.usgs.earthquake.nshmp.calc.Vs30;
-import gov.usgs.earthquake.nshmp.gmm.Imt;
-import gov.usgs.earthquake.nshmp.internal.Parsing;
-import gov.usgs.earthquake.nshmp.internal.Parsing.Delimiter;
-import gov.usgs.earthquake.nshmp.www.meta.Region;
 
 public enum Model {
 
-  AK_2007(
-      EnumSet.of(PGA, SA0P1, SA0P2, SA0P3, SA0P5, SA1P0, SA2P0),
-      EnumSet.of(VS_760)),
+  AK_2007(Set.of(BaseModel.AK_2007)),
+  CONUS_2008(Set.of(BaseModel.CEUS_2008, BaseModel.WUS_2008)),
+  CONUS_2014(Set.of(BaseModel.CEUS_2014, BaseModel.WUS_2014)),
+  CONUS_2014b(Set.of(BaseModel.CEUS_2014, BaseModel.WUS_2014B)),
+  CONUS_2018(Set.of(BaseModel.CEUS_2018, BaseModel.WUS_2018)),
+  HI_2020(Set.of(BaseModel.HI_2020));
 
-  CEUS_2008(
-      EnumSet.of(PGA, SA0P1, SA0P2, SA0P3, SA0P5, SA1P0, SA2P0),
-      EnumSet.of(VS_760, VS_2000)),
+  private Set<BaseModel> models;
 
-  WUS_2008(
-      EnumSet.of(PGA, SA0P1, SA0P2, SA0P3, SA0P5, SA0P75, SA1P0, SA2P0, SA3P0),
-      EnumSet.of(VS_1150, VS_760, VS_537, VS_360, VS_259, VS_180)),
-
-  CEUS_2014(
-      EnumSet.of(PGA, SA0P1, SA0P2, SA0P3, SA0P5, SA1P0, SA2P0),
-      EnumSet.of(VS_760, VS_2000)),
-
-  WUS_2014(
-      EnumSet.of(PGA, SA0P1, SA0P2, SA0P3, SA0P5, SA0P75, SA1P0, SA2P0, SA3P0),
-      EnumSet.of(VS_1150, VS_760, VS_537, VS_360, VS_259, VS_180)),
-
-  WUS_2014B(
-      EnumSet.of(PGA, SA0P1, SA0P2, SA0P3, SA0P5, SA0P75, SA1P0, SA2P0, SA3P0, SA4P0, SA5P0),
-      EnumSet.of(VS_1150, VS_760, VS_537, VS_360, VS_259, VS_180)),
-
-  CEUS_2018(
-      EnumSet.of(PGA, SA0P1, SA0P2, SA0P3, SA0P5, SA0P75, SA1P0, SA2P0, SA3P0, SA4P0, SA5P0),
-      EnumSet.of(VS_1150, VS_760, VS_537, VS_360, VS_259, VS_180)),
-
-  WUS_2018(
-      EnumSet.of(PGA, SA0P1, SA0P2, SA0P3, SA0P5, SA0P75, SA1P0, SA2P0, SA3P0, SA4P0, SA5P0),
-      EnumSet.of(VS_1150, VS_760, VS_537, VS_360, VS_259, VS_180)),
-
-  HI_2020(
-      EnumSet.of(PGA, SA0P1, SA0P2, SA0P3, SA0P5, SA0P75, SA1P0, SA2P0, SA3P0, SA5P0),
-      EnumSet.of(VS_1150, VS_760, VS_537, VS_360, VS_259, VS_180));
-
-  private static final String MODEL_DIR = "models";
-
-  public final Set<Imt> imts;
-  public final Set<Vs30> vs30s;
-
-  public final String path;
-  public final String name;
-  public final Region region;
-  public final String year;
-
-  private Model(Set<Imt> imts, Set<Vs30> vs30s) {
-    this.imts = Sets.immutableEnumSet(imts);
-    this.vs30s = Sets.immutableEnumSet(vs30s);
-    region = deriveRegion(name());
-    year = name().substring(name().lastIndexOf('_') + 1);
-    path = Paths.get(MODEL_DIR)
-        .resolve(region.name().toLowerCase())
-        .resolve(year.toLowerCase())
-        .toString();
-    name = Parsing.join(
-        ImmutableList.of(year, region.label, "Hazard Model"),
-        Delimiter.SPACE);
+  private Model(Set<BaseModel> models) {
+    this.models = models;
   }
 
-  private static Region deriveRegion(String s) {
-    return s.startsWith("AK") ? AK : s.startsWith("WUS") ? WUS : s.startsWith("HI") ? HI : CEUS;
+  public Set<BaseModel> models() {
+    return Set.copyOf(models);
   }
+
 }
