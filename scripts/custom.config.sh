560039fb2b27cd932534e83c9a7614b0ba140622
bclayton-usgs <bclayton@usgs.gov>
2020-04-07 13:51:33 -0600

    update context path

--- scripts/custom.config.sh
+++ scripts/custom.config.sh
@@ -1,5 +1,5 @@
 export SERVICE_MAP=(
-  "/nshmp/conus-2018":"nshmp-haz-conus-2018"
+  "/nshmp/ws/conus-2018":"nshmp-haz-conus-2018"
   # "/nshmp/conus-2014":"nshmp-haz-conus-2014"
   # "/nshmp/conus-2014b":"nshmp-haz-conus-2014b"
   # "/nshmp/conus-2008":"nshmp-haz-conus-2008"
