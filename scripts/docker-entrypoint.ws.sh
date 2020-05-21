9c9092623a7d6bb7d6172f2dc8bbf7de12d9941c
bclayton-usgs <bclayton@usgs.gov>
2020-05-21 16:26:23 -0600

    cleanup

--- scripts/docker-entrypoint.ws.sh
+++ scripts/docker-entrypoint.ws.sh
@@ -6,14 +6,14 @@ exit_status=${?};
 [ "${exit_status}" -eq 0 ] || exit "${exit_status}";
 
 # Download models to use
-model_path=$(get_models "${MODEL}" "${NSHM_VERSION}");
+get_models "${MODEL}" "${NSHM_VERSION}";
 exit_status=${?};
 check_exit_status ${exit_status};
 
 # Run web services
 java -jar "${PROJECT}-ws.jar" \
     "-Dmicronaut.server.context-path=${CONTEXT_PATH}" \
-    --model="/app/${model_path}";
+    --model="${MODEL}";
 exit_status=${?};
 check_exit_status ${exit_status};
 
