142366412062451976762f431a9a0e7cc7b6b561
Brandon Clayton <bclayton@usgs.gov>
2021-06-10 10:47:32 -0600

    add swagger service

---
+++ src/main/java/gov/usgs/earthquake/nshmp/www/SwaggerController.java
@@ -0,0 +1,47 @@
+package gov.usgs.earthquake.nshmp.www;
+
+import java.nio.charset.StandardCharsets;
+import java.util.stream.Collectors;
+
+import javax.inject.Inject;
+
+import com.google.common.io.Resources;
+
+import gov.usgs.earthquake.nshmp.www.services.ServicesUtil;
+
+import io.micronaut.http.HttpRequest;
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.MediaType;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+import io.swagger.v3.oas.annotations.Hidden;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+/**
+ * Expose OpenAPI YAML file.
+ *
+ * @author U.S. Geological Survey
+ */
+@Tag(
+    name = "Swagger",
+    description = "Swagger OpenAPI YAML")
+@Hidden
+@Controller("/swagger")
+public class SwaggerController {
+
+  @Inject
+  private NshmpMicronautServlet servlet;
+
+  @Get(produces = MediaType.TEXT_EVENT_STREAM)
+  public HttpResponse<String> doGet(HttpRequest<?> request) {
+    try {
+      var url = Resources.getResource("META-INF/swagger/nshmp-haz.yml");
+      var yml = Resources.readLines(url, StandardCharsets.UTF_8)
+          .stream()
+          .collect(Collectors.joining("\n"));
+      return HttpResponse.ok(yml);
+    } catch (Exception e) {
+      return ServicesUtil.handleError(e, "Swagger", request.getUri().getPath());
+    }
+  }
+}
