ec45a4515b71f89c3ea69bc1941b5f5a0610808b
Brandon Clayton <bclayton@usgs.gov>
2021-06-10 10:47:13 -0600

    update swagger

--- src/main/resources/swagger/index.js
+++ src/main/resources/swagger/index.js
@@ -3,40 +3,22 @@ window.onload = function() {
     return decodeURIComponent(
         v.replace(
           /(?:(?:^|.*;\s*)contextPath\s*\=\s*([^;]*).*$)|^.*$/,
-          "$1"
+          '$1'
         )
       );
-    },
-    cookie = extract(document.cookie),
-    contextPath = cookie === "" ? extract(window.location.search.substring(1)) : cookie,
-    f = contextPath === "" ? undefined : function(system) {
-      return {
-        statePlugins: {
-          spec: {
-            wrapActions: {
-              updateJsonSpec: (oriAction, system) => (...args) => {
-                let [spec] = args;
-                if (spec && spec.paths) {
-                  const newPaths = {};
-                  Object.entries(spec.paths)
-                      .forEach(([path, value]) => (newPaths[contextPath + path] = value));
-                  spec.paths = newPaths;
-                }
-                oriAction(...args);
-              }
-            }
-          }
-        }
-      };
-    },
-    ui = SwaggerUIBundle({
-      url: contextPath + "/swagger/nshmp-haz-openapi.yml",
-      dom_id: "#swagger-ui",
-      tagsSorter: 'alpha',
-      presets: [SwaggerUIBundle.presets.apis, SwaggerUIStandalonePreset],
-      plugins: [SwaggerUIBundle.plugins.DownloadUrl, f],
-      validatorUrl: null,
-      deepLinking: true
-    });
+  };
+
+  const contextPath = extract(document.cookie);
+
+  const ui = SwaggerUIBundle({
+    url: `${contextPath}/swagger`,
+    dom_id: '#swagger-ui',
+    tagsSorter: 'alpha',
+    presets: [SwaggerUIBundle.presets.apis, SwaggerUIStandalonePreset],
+    plugins: [SwaggerUIBundle.plugins.DownloadUrl],
+    validatorUrl: null,
+    deepLinking: true
+  });
+
   window.ui = ui;
 };
