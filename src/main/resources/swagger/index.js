7e306206049397ff0f313ab2522392a55de2fb77
Brandon Clayton <bclayton@usgs.gov>
2021-06-30 10:52:43 -0600

    update swagger

--- src/main/resources/swagger/index.js
+++ src/main/resources/swagger/index.js
@@ -1,24 +1,38 @@
 window.onload = function() {
-  const extract = function(v) {
-    return decodeURIComponent(
-        v.replace(
-          /(?:(?:^|.*;\s*)contextPath\s*\=\s*([^;]*).*$)|^.*$/,
-          '$1'
-        )
-      );
-  };
-
-  const contextPath = extract(document.cookie);
+  let contextPath = window.location.pathname;
+  contextPath = contextPath.endsWith('/') ? contextPath.slice(0, -1) : contextPath;
 
   const ui = SwaggerUIBundle({
-    url: `${contextPath}/swagger`,
+    url: `./swagger`,
     dom_id: '#swagger-ui',
     tagsSorter: 'alpha',
     presets: [SwaggerUIBundle.presets.apis, SwaggerUIStandalonePreset],
-    plugins: [SwaggerUIBundle.plugins.DownloadUrl],
+    plugins: [SwaggerUIBundle.plugins.DownloadUrl, updateContextPath(contextPath)],
     validatorUrl: null,
     deepLinking: true
   });
 
   window.ui = ui;
 };
+
+function updateContextPath(contextPath) {
+  return {
+    statePlugins: {
+      spec: {
+        wrapActions: {
+          updateJsonSpec: (oriAction) => (...args) => {
+            const [spec] = args;
+            if (spec && spec.paths) {
+              const newPaths = {};
+              Object.entries(spec.paths).forEach(
+                ([path, value]) => (newPaths[contextPath + path] = value)
+              );
+              spec.paths = newPaths;
+            }
+            oriAction(...args);
+          },
+        },
+      },
+    },
+  };
+}
