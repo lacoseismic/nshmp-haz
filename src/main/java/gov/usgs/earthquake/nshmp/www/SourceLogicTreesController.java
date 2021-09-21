27451557a367c10cd3e984fac47cfd1935a11fd3
Brandon Clayton <bclayton@usgs.gov>
2021-09-21 13:36:40 -0600

    Rename, add service methods

---
+++ src/main/java/gov/usgs/earthquake/nshmp/www/SourceLogicTreesController.java
@@ -0,0 +1,51 @@
+package gov.usgs.earthquake.nshmp.www;
+
+import javax.inject.Inject;
+
+import gov.usgs.earthquake.nshmp.www.services.SourceLogicTreesService;
+
+import io.micronaut.http.HttpRequest;
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.PathVariable;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(
+    name = "Source Logic Trees",
+    description = "NSHM source logic trees service")
+@Controller("/trees")
+public class SourceLogicTreesController {
+
+  @Inject
+  private NshmpMicronautServlet servlet;
+
+  @Operation(
+      description = "Returns the tectonic setting to source logic trees in the NSHM",
+      operationId = "trees_doGetMetadata",
+      summary = "Hazard model source logic trees")
+  @ApiResponse(
+      description = "Source logic trees metadata",
+      responseCode = "200")
+  @Get
+  public HttpResponse<String> doGetMetadata(HttpRequest<?> request) {
+    return SourceLogicTreesService.handleDoGetMetadata(request);
+  }
+
+  /**
+   * @param id Source tree id
+   */
+  @Operation(
+      description = "Returns the source logic tree for an id",
+      operationId = "trees_goGetTrees",
+      summary = "Get NSHM source logic tree")
+  @ApiResponse(
+      description = "NSHM source logic tree",
+      responseCode = "200")
+  @Get(uri = "/{id}")
+  public HttpResponse<String> doGetTrees(HttpRequest<?> request, @PathVariable int id) {
+    return SourceLogicTreesService.handleDoGetTrees(request, id);
+  }
+}
