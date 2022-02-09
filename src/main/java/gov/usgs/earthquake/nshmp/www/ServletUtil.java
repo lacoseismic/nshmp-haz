29f2b5a0d5854bfc31305d72e33700c91957bcdd
Peter Powers <pmpowers@usgs.gov>
2022-02-09 09:49:54 -0700

    spotless edits

--- src/main/java/gov/usgs/earthquake/nshmp/www/ServletUtil.java
+++ src/main/java/gov/usgs/earthquake/nshmp/www/ServletUtil.java
@@ -34,6 +34,7 @@ import gov.usgs.earthquake.nshmp.gmm.Imt;
 import gov.usgs.earthquake.nshmp.model.HazardModel;
 import gov.usgs.earthquake.nshmp.model.SiteData;
 import gov.usgs.earthquake.nshmp.www.meta.MetaUtil;
+
 import io.micronaut.context.annotation.Value;
 import io.micronaut.context.event.ShutdownEvent;
 import io.micronaut.context.event.StartupEvent;
