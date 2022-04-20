79cc4a2e7c93e57aa3619049fc1780d5edeb9575
Brandon Clayton <bclayton@usgs.gov>
2022-04-20 12:52:32 -0600

    add method to return all versions

--- src/main/java/gov/usgs/earthquake/nshmp/www/HazVersion.java
+++ src/main/java/gov/usgs/earthquake/nshmp/www/HazVersion.java
@@ -3,8 +3,18 @@ package gov.usgs.earthquake.nshmp.www;
 import com.google.common.io.Resources;
 
 import gov.usgs.earthquake.nshmp.internal.AppVersion;
+import gov.usgs.earthquake.nshmp.internal.LibVersion;
 
 public class HazVersion implements AppVersion {
+
+  public static VersionInfo[] appVersions() {
+    VersionInfo[] versions = {
+      new HazVersion().getVersionInfo(),
+      new LibVersion().getVersionInfo(),
+      new WsUtilsVersion().getVersionInfo(),
+    };
+    return versions;
+  }
 
   public VersionInfo getVersionInfo() {
     var resource = Resources.getResource("version/nshmp-haz-version.json");
